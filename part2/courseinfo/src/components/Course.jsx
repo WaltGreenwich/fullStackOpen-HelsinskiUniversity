3bc79692251a7ac7a4698e688fe45ae24ff80796
WGreenwichChaca <walter.greenwich@gmail.com>
2025-02-22 08:01:24 -0500

    Add part2 2.1 and 2.2

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,43 @@
+const Course = ({}) => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
+      },
+    ],
+  };
+
+  const { id, name, parts } = course;
+  return (
+    <div>
+      <h1>{name}</h1>
+
+      {parts.map((part) => (
+        <p key={part.id}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default Course;
